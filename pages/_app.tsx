9e46628408b30a6e5da918e58b8f795a10dce5ce
Kacper <k.kustra100@gmail.com>
2023-02-14 17:59:22 +0100

    Configure app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Lato } from '@next/font/google';
+
+const lato = Lato({
+  weight: ['400', '700', '900'],
+  variable: '--font-lato',
+  subsets: ['latin-ext'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${lato.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
